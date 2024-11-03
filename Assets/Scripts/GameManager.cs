30f3b4cd4aebd7f9a99fd79964988050c6eaf141
iamxray <a@kct.me>
2024-11-03 18:55:30 +0000

    Enemies Pooling v..1
    First try to make enemy pooling in GameManager.
    Enemies on edge reset position, rotation and set to off(and back to pooling pool).

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
+    GameObject player;
+
+    List<GameObject> enemies;
+
+    [SerializeField]
+
+    public GameObject enemiesToPool;
+
+    public int amountToPool = 10;
+
+
+
+
+
+    Vector3 spawnPoint = new Vector3(25,1,0);
+
+    float  endPointX = -25.0f;
+
+    [SerializeField]
     float secondsStart = 30.0f ;
+
 
     [SerializeField]
     float secondsLeft;
+
+
 
 
     public float SecondsLeft { get { return secondsLeft; } }
@@ -14,14 +40,38 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        enemies = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < amountToPool; i++)
+        {
+            tmp = Instantiate(enemiesToPool);
+            tmp.SetActive(false);
+            enemies.Add(tmp);
+        }
+
+        enemies[0].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         secondsLeft = secondsStart - Time.timeSinceLevelLoad;
-        
+
+        foreach (GameObject enemy in enemies) 
+        {
+            
+            
+            if (enemy.transform.position.x < endPointX)
+            {
+                enemy.transform.position = spawnPoint;
+                enemy.transform.rotation = Quaternion.identity;
+                enemy.SetActive(false);
+                
+            }
+        }
+        if (!enemies[0].activeInHierarchy) {
+            enemies[0].SetActive(true);
+        }
 
         
     }
