841b1e19b3df5b51f9b6a31108b4fdbf52fc601e
iamxray <a@kct.me>
2024-11-03 17:02:30 +0000

    FirstCommit
    Prototype start.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    Text timer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
